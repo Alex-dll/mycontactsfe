ad8cbe6fd254f2180b015d3a8de4d4e631008311
Alex-dll <alexsandrojr.303@gmail.com>
2022-01-18 14:55:29 -0300

    set global styles

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from '../../assets/styles/global';
+import defaultTheme from '../../assets/styles/themes/default';
+
+import { Container } from './styles';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Container>My contacts</Container>
+    </ThemeProvider>
+  );
+}
