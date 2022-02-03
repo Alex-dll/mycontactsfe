86615be7d93e236cc12cd5aafd4d94634fcc7160
Alex-dll <alexsandrojr.303@gmail.com>
2022-02-03 14:14:39 -0300

    add ContactsList

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,17 @@
 export default {
-  backgroundColor: '#F6F5FC',
+  colors: {
+    background: '#F6F5FC',
+    primary: {
+      main: '#5061FC',
+      lighter: '#E0E3FF',
+    },
+    danger: {
+      main: '#FC5050',
+    },
+    gray: {
+      900: '#222222',
+      200: '#BCBCBC',
+      100: '#E6E6E6',
+    },
+  },
 };
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
+import ContactsList from '../ContactsList';
 import Header from '../Header';
 
 import { Container } from './styles';
@@ -11,8 +12,10 @@ export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
+
       <Container>
         <Header />
+        <ContactsList />
       </Container>
     </ThemeProvider>
   );
