f1f36a8797c113dd6e44bcb3c4071416dba906b8
alex-dll <alexsandrojr.303@gmail.com>
2022-02-03 23:47:46 -0300

    add a Home Page and refactor a Header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,92 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  Container, InputSearchContainer, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 function Home() {
-  return <div />;
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Alex Sandro</strong>
+              <small>instagram</small>
+            </div>
+            <span>alex@gmail.com</span>
+            <span>(41) 99999-99999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Alex Sandro</strong>
+              <small>instagram</small>
+            </div>
+            <span>alex@gmail.com</span>
+            <span>(41) 99999-99999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Alex Sandro</strong>
+              <small>instagram</small>
+            </div>
+            <span>alex@gmail.com</span>
+            <span>(41) 99999-99999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
 
 export default Home;
