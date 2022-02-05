142e8bbcbdd3b28445b3681d16667b78e0bea01c
alex-dll <alexsandrojr.303@gmail.com>
2022-02-05 13:03:29 -0300

    created components Input and Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 // import { Container } from './styles';
 
 function NewContact() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
 
